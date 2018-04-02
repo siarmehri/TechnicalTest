6d6dff43bc0311faf37e6671e93ef604c2825e45
siar <siarmehri.angel@gmail.com>
2018-04-02 23:43:57 +0100

    Added placeholders for createNewRecord inputs...

--- resources/views/partials/createNewRecord.blade.php
+++ resources/views/partials/createNewRecord.blade.php
@@ -1,7 +1,7 @@
 <tr>
-    <td><input type="text" name="firstname" value="" /></td>
-    <td><input type="text" name="lastname" value="" /></td>
-    <td><input type="text" name="email" value="" /></td>
-    <td><input type="text" name="job_role" value="" /></td>
-    <td><input type="button" value="submit" /></td>
+    <td><input type="text" name="firstname" value="" placeholder="Add New..."/></td>
+    <td><input type="text" name="lastname" value="" placeholder="Add New..."/></td>
+    <td><input type="text" name="email" value="" placeholder="Add New..."/></td>
+    <td><input type="text" name="job_role" value="" placeholder="Add New..."/></td>
+    <td><input type="button" value="submit" placeholder="Add New..."/></td>
 </tr>
