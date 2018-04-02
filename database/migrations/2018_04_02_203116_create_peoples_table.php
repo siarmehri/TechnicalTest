ddaffe2e38ccede662e0ee93b886204f525b66c9
siar <siarmehri.angel@gmail.com>
2018-04-02 22:33:33 +0100

    changed table name from people to peoples it should be plural name

--- database/migrations/2018_04_02_203116_create_peoples_table.php
+++ database/migrations/2018_04_02_203116_create_peoples_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreatePeopleTable extends Migration
+class CreatePeoplesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,7 +13,7 @@ class CreatePeopleTable extends Migration
      */
     public function up()
     {
-        Schema::create('people', function (Blueprint $table) {
+        Schema::create('peoples', function (Blueprint $table) {
             $table->increments('id');
             $table->string('firstname', 50);
             $table->string('lastname', 50);
