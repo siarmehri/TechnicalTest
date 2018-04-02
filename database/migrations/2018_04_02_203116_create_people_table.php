2d3930357a4a18f41fd9f638eeaaa056e82a32a3
siar <siarmehri.angel@gmail.com>
2018-04-02 21:58:56 +0100

    migration being added I have defaulted updated_at and created_at as the laravel timestamps don't do this...

---
+++ database/migrations/2018_04_02_203116_create_people_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePeopleTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('people', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('firstname', 50);
+            $table->string('lastname', 50);
+            $table->string('email', 50)->unique();
+            $table->string('job_role', 50);
+            $table->timestamp('created_at')->default(\Illuminate\Support\Facades\DB::raw('CURRENT_TIMESTAMP'));
+            $table->timestamp('updated_at')->default(\Illuminate\Support\Facades\DB::raw('CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP'));;
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('people');
+    }
+}
--- readme.md
+++ readme.md
@@ -1,2 +1,12 @@
 # Installed 
 composer create-project --prefer-dist laravel/laravel TechnicalTest
+# Created Controller Factory Model for peoples
+php artisan make:model -cfm People
+# people table
+I have decided to have one table as I know that the job_role could go to a separate table.
+For this certain test which have the maximum of 10 records with a condition to not allow more than 4 records to have same job_role, 
+the relationship will complicate the task and we will need to add more logic to enforce our condition.
+Even though it could be done by adding counter to job_role table to show how many persons do we have in this certain role, but updating the counter would be a bit annoying. 
+With this solution we can easily count our condition with a simple query.
+
+
