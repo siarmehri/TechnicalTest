6f947126f388824214c19ce589e4325afea59f53
siar <siarmehri.angel@gmail.com>
2018-04-05 01:20:12 +0100

    added maxlength for inputs...

--- resources/views/partials/createNewRecord.blade.php
+++ resources/views/partials/createNewRecord.blade.php
@@ -1,7 +1,7 @@
 <tr>
-    <td><input type="text" name="firstname" class="new" value="" placeholder="Add New..."/></td>
-    <td><input type="text" name="lastname" class="new" value="" placeholder="Add New..."/></td>
-    <td><input type="text" name="email" class="new" value="" placeholder="Add New..."/></td>
-    <td><input type="text" name="job_role" class="new" value="" placeholder="Add New..."/></td>
+    <td><input type="text" name="firstname" class="new" value="" maxlength="50" placeholder="Add New..."/></td>
+    <td><input type="text" name="lastname" class="new" value="" maxlength="50" placeholder="Add New..."/></td>
+    <td><input type="text" name="email" class="new" value="" maxlength="50" placeholder="Add New..."/></td>
+    <td><input type="text" name="job_role" class="new" value="" maxlength="50" placeholder="Add New..."/></td>
     <td><input type="button" value="submit" class="addNew" placeholder="Add New..."/></td>
 </tr>
